4c3f956526c3a006be0e5b304addc3f0ef9e142e
Ultramann <cary.goltermann@gmail.com>
2016-11-12 15:22:50 -0800

    Change parser functions to class. Now handling lineup changes

--- data_prep.py
+++ data_prep.py
@@ -1,15 +1,14 @@
 from pymongo import MongoClient
 
-
-'''Notes
-* Successful 3 pointers are fg (1) but say '3PT' in them
-* Dunks are fg (1), but say 'Dunk' in them
-* Both offensive and defensive rebounds are coded 5. Only way to determine
-    which one is by looking at the diff on '(Off: #, Def:#)' which corresponds
-    to that player at that point in the game. This will be a pain...
-* Quarter is stored under the 'PERIOD' key in each event.
-* The person who is fouled (6) is listed under player 2
-'''
+'''Notes:
+    * Successful 3 pointers are fg (1) but say '3PT' in them
+    * Dunks are fg (1), but say 'Dunk' in them
+    * Both offensive and defensive rebounds are coded 5. Only way to determine
+        which one is by looking at the diff on '(Off: #, Def:#)' which corresponds
+        to that player at that point in the game. This will be a pain...
+    * Quarter is stored under the 'PERIOD' key in each event.
+    * The person who is fouled (6) is listed under player 2
+    '''
 
 EVENT_DICT = {1: 'Field Goal',
               2: 'Missed FG',
@@ -26,78 +25,114 @@ EVENT_DICT = {1: 'Field Goal',
               13: 'Period End'}
 
 
-def make_play_list(game):
-    """Turn a game dictionary in to a list of dictionaries.
+class ParseGameJSON():
+    def __init__(self, game):
+        self.game = game
+        self.plays = []
+        self._players = self._make_players_dict()
+        self._current_players = self._players[0]
+        self._parse_plays()
 
-    Parameters
-    ----------
-    game : dict - lots of nested stuff...
+    def _parse_plays(self):
+        """Turn a game dictionary in to a list of lists. Appends information
+        from game events to plays attribute.
+        """
+        events = self.game['_playbyplay']['resultSets']['PlayByPlay']
+        for event in events:
+            play = self._parse_event(event)
+            if play == 'stop':
+                break
+            if play:
+                self.plays.append(play)
 
-    Returns
-    -------
-    List of lists - inner list represents play.
-    """
-    players = make_starting_players(game)
-    events = game['_playbyplay']['resultSets']['PlayByPlay']
-    plays = []
-    for event in events:
-        play = parse_event(players, event)
-        if play:
-            plays.append(play)
-    return plays
+    def _make_players_dict(self):
+        """Makes dictionary of players on the court.
+
+        Returns
+        -------
+        {event_num: [id, name] * 10}
+        """
+        def get_players(matchup):
+            """Helper function to make player list from matchup.
+            """
+            home_players = [p[attr] for p in matchup['home_players'][0]
+                                    for attr in ['id', 'name']]
+            away_players = [p[attr] for p in matchup['away_players'][0]
+                                    for attr in ['id', 'name']]
+            return home_players + away_players
+        
+        return {matchup['start_id']: get_players(matchup)
+                                     for matchup in self.game['matchups']}
+
+    def _parse_event(self, event):
+        """Turn an event dictionary into a list that's better for putting
+        in a dataframe.
+
+        Parameters
+        ----------
+        event : dict
+        """
+        if event['EVENTMSGTYPE'] == 12:
+            self._current_players = self._players.get(event['EVENTNUM'],
+                                                      self._current_players)
+        if event['EVENTMSGTYPE'] == 8:
+            stop = self._update_current_players(event)
+            return stop
+        event_list = [event['EVENTNUM']] + self._current_players[:]
+        event_list.append(event['PCTIMESTRING'])
+        event_list.append(event['EVENTMSGTYPE'])
+        event_list.extend([event['PLAYER1_ID'], event['PLAYER1_NAME']])
+        event_list.extend([event['PLAYER2_ID'], event['PLAYER2_NAME']])
+        event_list.append(event['HOMEDESCRIPTION'] if event['HOMEDESCRIPTION'] \
+                                                   else event['VISITORDESCRIPTION'])
+        return event_list
+
+    def _update_current_players(self, event):
+        """Turn an event dictionary into a list that's better for putting in a dataframe.
+
+        Parameters
+        ----------
+        event : dict
+        """
+        player_1_idx = self._current_players.index(event['PLAYER1_ID'])
+        sub_player_info = event['PLAYER2_ID'], event['PLAYER2_NAME']
+        self._current_players[player_1_idx:player_1_idx+2] = sub_player_info
 
 
-def make_starting_players(game):
-    """Makes list of starting player id-name tuples.
-
-    Parameters
-    ----------
-    game : dict - lots of nested stuff...
-
-    Returns
-    -------
-    [[id, name]]
-    """
-    home_starters = [[p['id'], p['name']] for p in game['home_starters']]
-    away_starters = [[p['id'], p['name']] for p in game['away_starters']]
-    return home_starters + away_starters 
-
-
-def parse_event(players, event):
-    """Turn an event dictionary into a list that's better for putting in a dataframe.
-
-    Parameters
-    ----------
-    players : list - [[id, name]], current players at start of event
-    event : dict
-    """
-    event_list = [attr for player in players for attr in player]
-    event_list.append(event['PCTIMESTRING'])
-    event_list.append(event['EVENTMSGTYPE'])
-    event_list.extend([event['PLAYER1_ID'], event['PLAYER1_NAME']])
-    event_list.extend([event['PLAYER2_ID'], event['PLAYER2_NAME']])
-    event_list.append(event['HOMEDESCRIPTION'] if event['HOMEDESCRIPTION'] \
-                                               else event['VISITORDESCRIPTION'])
-    return event_list
-
-
-def view_plays(game):
+def view_plays(game, only_event_type=None):
     """Helper function to inspect what the data in a game looks like.
-    
+
     Parameters
     ----------
     game : dict
+    only_event_type : int - only show this event type
     """
+    def make_description(event):
+        """Helper function to handling interactive printing.
+        """
+        if event['VISITORDESCRIPTION']:
+            return event['VISITORDESCRIPTION']
+        elif event['HOMEDESCRIPTION']:
+            return event['HOMEDESCRIPTION']
+        else:
+            return event['PCTIMESTRING']
+
     events = game['_playbyplay']['resultSets']['PlayByPlay']
-    for event in events: 
-        description = event['HOMEDESCRIPTION'] if event['HOMEDESCRIPTION'] \
-                                               else event['VISITORDESCRIPTION']
-        print('{} - {}'.format(description, event['EVENTMSGTYPE']))
-        if input() == ' ':
-            print(event)
-            print('\n')
+    for event in events:
+        if not only_event_type or event['EVENTMSGTYPE'] == only_event_type:
+            description = make_description(event)
+            print('{: <3} :: {: <70} Type = {: <2} Period = {}'.format(
+                   event['EVENTNUM'], description,
+                   event['EVENTMSGTYPE'], event['PERIOD']))
+            inpt = input()
+            if inpt == 'i':
+                print(event)
+                print('\n')
+            elif inpt == 'q':
+                break
 
 
 if __name__ == '__main__':
     client = MongoClient()
     games_col = client.bball.games
+    test_game = games_col.find()[0]
